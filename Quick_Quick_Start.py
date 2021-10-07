592eea2b9a20ee4e2236437b04a1ffab6fc148e1
ggardiakos <69169466+ggardiakos@users.noreply.github.com>
2021-10-07 14:01:50 -0500

    Update Quick_Quick_Start.py

--- Quick_Quick_Start.py
+++ Quick_Quick_Start.py
@@ -10,8 +10,8 @@ print(mw.shash(write_key))
 
 print(mw.animal_from_key(write_key))
 
-import requests
 
+Import requests
 #uncomment to submit your email for prizes
 #res = requests.put('https://devapi.microprediction.org/email/4c7b09f29b8b7eb580b48ada674142cd', params={'email': 'YOUR EMAIL HERE'})
 res.json()
